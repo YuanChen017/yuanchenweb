45e5dc37cefeaf0768a3c468e731916bcf21d981
YuanChen <yuc017@ucsd.edu>
2023-07-17 14:37:12 -0700

    update home route

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Project />} />
         <Route path="/portfolio/calculate" element={<Calcut />} />
