fc186a3faf8856afb0e71665086273773efc0213
YuanChen <yuc017@ucsd.edu>
2023-07-18 23:35:57 -0700

    project updated

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -8,45 +8,9 @@ const Project = () => {
   const [dislike, setDislike] = useState(25);
   return (
     <div className="Project">
-      <div className="border border-primary m-2 p-2">
-        <article>
-          Various educators teach rules governing the length of paragraphs. They
-          may say that a paragraph should be 100 to 200 words long, or be no
-          more than five or six sentences. But a good paragraph should not be
-          measured in characters, words, or sentences. The true measure of your
-          paragraphs should be ideas.
-        </article>
-        <button
-          className="btn btn-primary"
-          onClick={() => {
-            if (isliked) {
-              setDislike(dislike + 1);
-              setLike(like - 1);
-            } else {
-              setLike(like + 1);
-              setDislike(dislike - 1);
-            }
-            setIsliked(!isliked);
-          }}
-        >
-          {isliked ? "liked" : "like"} | <span>{like}</span>
-        </button>
-        <button
-          className="btn btn-primary"
-          onClick={() => {
-            if (isdisliked) {
-              setDislike(dislike - 1);
-              setLike(like + 1);
-            } else {
-              setDislike(dislike + 1);
-              setLike(like - 1);
-            }
-            setIsdisliked(!isdisliked);
-          }}
-        >
-          {isdisliked ? "dislike" : "disliked"} | <span>{dislike}</span>
-        </button>
-      </div>
+      <a href="https://yuanchen017.github.io/ecommerceweb/" target="_blank">
+        Ecommerce web application
+      </a>
       <div className="calculate m-2 p-2">
         <Link to="/portfolio/calculate">redirect to calculator PJ</Link>
       </div>
