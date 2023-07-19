d3e5427ab76c471312fa792c0eb1e5ba04c4fda2
YuanChen <yuc017@ucsd.edu>
2023-07-18 18:12:30 -0700

    1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Searchauto from "./components/searchauto/searchauto";
 import Footer from "./components/footer/footer";
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
