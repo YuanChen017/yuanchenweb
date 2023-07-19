4c9038fdce9f7ea8f976a63930971da37fb32698
YuanChen <yuc017@ucsd.edu>
2023-07-19 15:35:11 -0700

    form update

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -22,6 +22,7 @@ const Contact = () => {
               type="text"
               className="form-control"
               id="firstname"
+              name="firstname"
               required
             />
           </div>
@@ -33,6 +34,7 @@ const Contact = () => {
               type="text"
               className="form-control"
               id="lastname"
+              name="lastname"
               required
             />
           </div>
@@ -40,19 +42,35 @@ const Contact = () => {
             <label htmlFor="email" className="form-label">
               Email:
             </label>
-            <input type="text" className="form-control" id="email" required />
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              id="email"
+              required
+            />
           </div>
           <div className="col-md-4">
             <label htmlFor="phone" className="form-label">
               Phone:
             </label>
-            <input type="text" id="phone" className="form-control" />
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              className="form-control"
+            />
           </div>
           <div className="col-md-12">
             <label htmlFor="comment" className="form-label">
               Comments & Question:
             </label>
-            <input type="text" id="comment" className="form-control" />
+            <input
+              type="text"
+              id="comment"
+              name="comment"
+              className="form-control"
+            />
           </div>
           <div class="col-md-12">
             <button type="submit" class="btn btn-primary">
