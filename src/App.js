328ef2dba0914f17639f5c43eaa614d1f09c8e27
YuanChen <yuc017@ucsd.edu>
2023-07-18 18:42:41 -0700

    route fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/home/Home";
 import Project from "./components/project/project";
 import About from "./components//about/about";
