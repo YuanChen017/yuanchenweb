f9a424f136fbc50689c6356ffcec0f0726bf4c0c
unknown <yuc017@ucsd.edu>
2023-08-29 19:08:53 -0700

    navbar check

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,24 +1,47 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 import "./navbar.css";
 const Navbar = () => {
+  useEffect(() => {
+    if (window.innerWidth > 850) {
+      document.getElementsByClassName("menulinks")[0].style.display =
+        "inline-flex";
+    }
+  }, [window.innerWidth]);
+  const handleopenmenu = () => {
+    if (
+      window.innerWidth < 850 &&
+      document.getElementsByClassName("menulinks")[0].style.display === "none"
+    ) {
+      document.getElementsByClassName("menulinks")[0].style.display = "flex";
+    } else if (window.innerWidth > 850) {
+      document.getElementsByClassName("menulinks")[0].style.display =
+        "inline-flex";
+    } else {
+      document.getElementsByClassName("menulinks")[0].style.display = "none";
+    }
+  };
+
   return (
     <div className="header">
       <div className="leftheader">
         <div className="name">YUAN.C</div>
       </div>
+      <div className="bugermenu" onClick={handleopenmenu}>
+        &#9776;
+      </div>
       <div className="rightheader">
-        <ul>
-          <li>
+        <ul className="menulinks">
+          <li onClick={handleopenmenu}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li onClick={handleopenmenu}>
             <Link to="/about">About</Link>
           </li>
-          <li>
+          <li onClick={handleopenmenu}>
             <Link to="/contact">Contact Me</Link>
           </li>
-          <li>
+          <li onClick={handleopenmenu}>
             <Link to="/portfolio">Projects</Link>
           </li>
         </ul>
